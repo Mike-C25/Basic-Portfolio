7ed94c48598ff7bc5fd931321daed498faee7a55
Michael Chiang <michael.chiang25@gmail.com>
2017-07-27 13:14:48 -0400

    Added changes for css/html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,68 @@
-function getDictionaryValueBasedOnUrl(dict){
-	var lastPart = '';
-	var lastSlash = window.location.href.lastIndexOf("/");
-	var lastPeriod = '';
-	//console.log(lastSlash);
-	lastPart = window.location.href.substr(lastSlash + 1);
-	//console.log(lastPart);
-	lastPart = lastPart.substr(0, lastPart.lastIndexOf("."));
-	return dict[lastPart];
-}
-function doStuff(c){
-	$("#about").addClass(c[0], 
-	$("#connect").addClass(c[1]));
-
-	$("#portfolio").addClass(c[0], 
-	$("#connect").addClass(c[1]));
-
-	$("#form").addClass(c[0], 
-	$("#connect").addClass(c[1]));
+function getDictionaryValueBasedOnUrl(dict) {
+    var lastPart = '';
+    var lastSlash = window.location.href.lastIndexOf("/");
+    var lastPeriod = '';
+    //console.log(lastSlash);
+    lastPart = window.location.href.substr(lastSlash + 1);
+    //console.log(lastPart);
+    lastPart = lastPart.substr(0, lastPart.lastIndexOf("."));
+    return dict[lastPart];
 }
 
-$(document).ready(function(){ 
-	var pageTitle = {};
-	pageTitle.index = 'y';
-	pageTitle.portfolio = 'g';
-	pageTitle.contact = 'b';
-	var pageDict = {};
-	pageDict.index = function(){
-		doStuff(['animated fadeInUp', 'animated fadeInRight'])
-	};
-	pageDict.portfolio = function(){
-		doStuff(['animated fadeInUp', 'animated fadeInRight'])
-	};
-	pageDict.contact = function(){
-		doStuff(['animated fadeInUp', 'animated fadeInRight'])
-	};
+function doStuff(c) {
+    $("#about").addClass(c[0],
+        $("#connect").addClass(c[1]));
 
-	// //console.log(window.location);
-	// // [^/]+(?=/$|$)
-	// var lastPart = '';
-	// var lastSlash = window.location.href.lastIndexOf("/");
-	// var lastPeriod = '';
-	// //console.log(lastSlash);
-	// lastPart = window.location.href.substr(lastSlash + 1);
-	// //console.log(lastPart);
-	// lastPart = lastPart.substr(0, lastPart.lastIndexOf("."));
-	// //console.log(lastPart);
+    $("#portfolio").addClass(c[0],
+        $("#connect").addClass(c[1]));
 
-	// // console.log(pageTitle[lastPart]);
+    $("#form").addClass(c[0],
+        $("#connect").addClass(c[1]));
+}
 
-	// var colorvar = pageTitle[lastPart];
-	var colorvar = getDictionaryValueBasedOnUrl(pageTitle);
-	var stuff = getDictionaryValueBasedOnUrl(pageDict);
-	
+$(document).ready(function() {
+    $("body").removeClass("preload");
 
-	$(".title").addClass(colorvar);
-	$(".content-contain h3").addClass(colorvar);
-	$(".footer").addClass(colorvar);
 
-	stuff();
-	
+    var pageTitle = {};
+    pageTitle.index = 'y';
+    pageTitle.portfolio = 'g';
+    pageTitle.contact = 'b';
+    var pageDict = {};
+    pageDict.index = function() {
+        doStuff(['animated fadeInUp', 'animated fadeInRight'])
+    };
+    pageDict.portfolio = function() {
+        doStuff(['animated fadeInUp', 'animated fadeInRight'])
+    };
+    pageDict.contact = function() {
+        doStuff(['animated fadeInUp', 'animated fadeInRight'])
+    };
+
+    // //console.log(window.location);
+    // // [^/]+(?=/$|$)
+    // var lastPart = '';
+    // var lastSlash = window.location.href.lastIndexOf("/");
+    // var lastPeriod = '';
+    // //console.log(lastSlash);
+    // lastPart = window.location.href.substr(lastSlash + 1);
+    // //console.log(lastPart);
+    // lastPart = lastPart.substr(0, lastPart.lastIndexOf("."));
+    // //console.log(lastPart);
+
+    // // console.log(pageTitle[lastPart]);
+
+    // var colorvar = pageTitle[lastPart];
+    var colorvar = getDictionaryValueBasedOnUrl(pageTitle);
+    var stuff = getDictionaryValueBasedOnUrl(pageDict);
+
+
+    $(".title").addClass(colorvar);
+    $(".content-contain h3").addClass(colorvar);
+    $(".footer").addClass(colorvar);
+
+    stuff();
+
 
 
 
