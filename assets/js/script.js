e843d6cbdac1dbdf578d17aa9544ddf6b1fcf59e
Michael Chiang <michael.chiang25@gmail.com>
2017-07-26 20:52:41 -0400

    Added javascript functions for colors and content slides. Added CSS for colors/animate.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,4 @@
-$(document).ready(function(){
-	var pageTitle = {};
-	pageTitle.index = 'y';
-	pageTitle.portfolio = 'g';
-	pageTitle.contact = 'b';
-	//console.log(window.location);
-	// [^/]+(?=/$|$)
+function getDictionaryValueBasedOnUrl(dict){
 	var lastPart = '';
 	var lastSlash = window.location.href.lastIndexOf("/");
 	var lastPeriod = '';
@@ -12,11 +6,63 @@ $(document).ready(function(){
 	lastPart = window.location.href.substr(lastSlash + 1);
 	//console.log(lastPart);
 	lastPart = lastPart.substr(0, lastPart.lastIndexOf("."));
-	//console.log(lastPart);
+	return dict[lastPart];
+}
+function doStuff(c){
+	$("#about").addClass(c[0], 
+	$("#connect").addClass(c[1]));
 
-	// console.log(pageTitle[lastPart]);
+	$("#portfolio").addClass(c[0], 
+	$("#connect").addClass(c[1]));
 
-	var colorvar = pageTitle[lastPart];
+	$("#form").addClass(c[0], 
+	$("#connect").addClass(c[1]));
+}
+
+$(document).ready(function(){ 
+	var pageTitle = {};
+	pageTitle.index = 'y';
+	pageTitle.portfolio = 'g';
+	pageTitle.contact = 'b';
+	var pageDict = {};
+	pageDict.index = function(){
+		doStuff(['animated fadeInUp', 'animated fadeInRight'])
+	};
+	pageDict.portfolio = function(){
+		doStuff(['animated fadeInUp', 'animated fadeInRight'])
+	};
+	pageDict.contact = function(){
+		doStuff(['animated fadeInUp', 'animated fadeInRight'])
+	};
+
+	// //console.log(window.location);
+	// // [^/]+(?=/$|$)
+	// var lastPart = '';
+	// var lastSlash = window.location.href.lastIndexOf("/");
+	// var lastPeriod = '';
+	// //console.log(lastSlash);
+	// lastPart = window.location.href.substr(lastSlash + 1);
+	// //console.log(lastPart);
+	// lastPart = lastPart.substr(0, lastPart.lastIndexOf("."));
+	// //console.log(lastPart);
+
+	// // console.log(pageTitle[lastPart]);
+
+	// var colorvar = pageTitle[lastPart];
+	var colorvar = getDictionaryValueBasedOnUrl(pageTitle);
+	var stuff = getDictionaryValueBasedOnUrl(pageDict);
+	
+
 	$(".title").addClass(colorvar);
 	$(".content-contain h3").addClass(colorvar);
+	$(".footer").addClass(colorvar);
+
+	stuff();
+	
+
+
+
+
+
+
 });
